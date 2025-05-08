d9a4a5240dd32862500e54a3d67456e5446977a8
Ia-Fin <ian.finley@bellevuecollege.edu>
2025-05-08 22:53:04 +0000

    Corrected indents

--- m05/js/code.js
+++ m05/js/code.js
@@ -8,54 +8,54 @@ let userValue = prompt("Please provide a number between 0 and 10:", "");
 
 // If the user enters nothing, a non-number, or a number greater than 10 it sets their entered value as 0
 if (userValue === null || isNaN(userValue) || (userValue > 10)) { 
-  userValue = "0";
+	userValue = "0";
 } 
 
 // This updates the the table variable to be the number the user selected, 0 - 10.
 switch(userValue) { 
-     case "0":
+	case "0":
     table = 0;
     break; 
-  case "1":
+	case "1":
     table = 1;
     break;
-  case "2":
+	case "2":
     table = 2;
     break;
-  case "3":
+	case "3":
     table = 3;
     break;
-  case "4":
+	case "4":
     table = 4;
     break;	
-  case "5":
+	case "5":
     table = 5;
     break;
-  case "6":
+	case "6":
     table = 6;
     break;
-  case "7":
+	case "7":
     table = 7;
     break;
-  case "8":
+	case "8":
     table = 8;
     break;	
-  case "9":
+	case "9":
     table = 9;
     break;
-  case "10":
+	case "10":
     table = 10;
     break;
-  default:
+	default:
     table = 0;
 }
 
 // Perform the multiplication using a while loop to add a line to the msg with the updated calculation until there are 10 lines.
 if (operator === 'multiplication') {
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+	while (i < 11) {
+		msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+		i++;
+	}
 }
 
 // Writes the message into the page
