bac0769ec5e23bc7eaa9125d8fc45496f1ed6edf
Ia-Fin <ian.finley@bellevuecollege.edu>
2025-06-14 06:39:18 +0000

    Updated with countdown timer

--- a5/js/slideshow.js
+++ a5/js/slideshow.js
@@ -1,45 +1,63 @@
 var myImages =["images/cube.jpg", "images/glass.jpg","images/pyramid.jpg","images/shapes.jpg","images/sphere.jpg"];
 
-
 var captionImages =["CUBE","GLASS","PRYAMID","SHAPES","SPHERE"];
 
- var index=0; 
+var index = 0;
+var countdown = 4; // seconds
+var timerDisplay = document.getElementById("timer");
+var intervalId = null;
 
- function updateImage(){
- document.getElementById("slideshow").src = myImages[index];
- document.getElementById("slideshow").alt= captionImages[index];
- document.getElementById("caption").textContent = captionImages[index]; 
-} 
-
-function next(){
- if (myImages.length == index+1)
- index=0;
- else
- index++;
- updateImage();
-} 
- 
-
-function back(){
- if (index===0)
- index=myImages.length-1;
- else
- index--;
- 
- updateImage();
-} 
-
-var nextButton = document.getElementById("next"); 
-var previousButton = document.getElementById("previous"); 
-
-previousButton.addEventListener("click",back,false);
-nextButton.addEventListener("click",next,false); 
-
-
-function autoSlide(){
-if (document.getElementById("auto").checked)
- next(); 
+function updateImage() {
+	document.getElementById("slideshow").src = myImages[index];
+	document.getElementById("slideshow").alt = captionImages[index];
+	document.getElementById("caption").textContent = captionImages[index];
 }
 
+function next() {
+	if (myImages.length == index + 1)
+		index = 0;
+	else
+		index++;
+	updateImage();
+	resetCountdown();
+}
 
-setInterval(autoSlide,2000); // Next
+function back() {
+	if (index === 0)
+		index = myImages.length - 1;
+	else
+		index--;
+	updateImage();
+	resetCountdown();
+}
+
+function autoSlideTick() {
+	if (document.getElementById("auto").checked) {
+		countdown--;
+		timerDisplay.textContent = countdown;
+		if (countdown <= 0) {
+			next(); // resets countdown as part of next()
+		}
+	} else {
+		timerDisplay.textContent = "--";
+	}
+}
+
+function resetCountdown() {
+	if (document.getElementById("auto").checked) {
+		countdown = 4;
+		timerDisplay.textContent = countdown;
+	} else {
+		timerDisplay.textContent = "--";
+	}
+}
+
+// Buttons
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
+
+// Start interval timer
+intervalId = setInterval(autoSlideTick, 1000); // ticks every 1 second
