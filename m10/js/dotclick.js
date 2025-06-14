0e44af165647b73f27a2afa20bdba1b621b704fc
Ia-Fin <ian.finley@bellevuecollege.edu>
2025-06-14 04:13:14 +0000

    Updated with color and size change

--- m10/js/dotclick.js
+++ m10/js/dotclick.js
@@ -1,20 +1,29 @@
-var  mybutton = document.querySelector("button");
+var mybutton = document.querySelector("button");
+
 mybutton.addEventListener("click", function(event) {
+	var element = document.getElementsByClassName("dot");
+	for (let index = element.length - 1; index >= 0; index--) {
+		element[index].parentNode.removeChild(element[index]);
+	}
+	
+	// Let us stop the propagation of events
+	event.stopPropagation();
+});
 
-
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
-
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("div");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
+addEventListener("click", function(event) {
+	var dot = document.createElement("div");
+	dot.className = "dot";
+	
+	// Get user-selected color and size
+	var color = document.getElementById("dotColor").value;
+	var size = parseInt(document.getElementById("dotSize").value) || 8;
+	
+	dot.style.width = size + "px";
+	dot.style.height = size + "px";
+	dot.style.background = color;
+	dot.style.borderRadius = (size / 2) + "px";
+	dot.style.left = (event.pageX - size / 2) + "px";
+	dot.style.top = (event.pageY - size / 2) + "px";
+	
+	document.body.appendChild(dot);
+});
